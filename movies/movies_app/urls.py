dac6808297b341e21dacd8386571335e830e01ee
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-25 17:29:00 -0400

    changed path to list.

--- movies/movies_app/urls.py
+++ movies/movies_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
 from movies_app.views import movie_list
 urlpatterns = [
-    path('movie/', include(movie_app.urls)),
+    path('list/', include(movie_app.urls)),
 ]
