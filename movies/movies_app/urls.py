eb02df46381c5cdad67fee09b946c9a695e4fd2b
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-25 19:22:22 -0400

    modified patterns.

--- movies/movies_app/urls.py
+++ movies/movies_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from movies_app.views import movie_list
 urlpatterns = [
-    path('list/', movie_list, name='movie-list'),
+    path('admin/', admin.site.urls),
+    path('movie/', include('movies_app.urls')),
 ]
