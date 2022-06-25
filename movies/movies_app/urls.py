7930bffdcdcf4f77060fdb5d43d61d2402c65631
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-25 17:25:01 -0400

    Changed name of app.

--- movies/movies_app/urls.py
+++ movies/movies_app/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('movie/', include(watchlist_app.urls)),
+    path('movie/', include(movie_app.urls)),
 ]
