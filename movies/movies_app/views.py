2bcbefe24859e0068e4e3cca7f3bbaccc65c0830
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-25 17:00:10 -0400

    modified comment.

--- movies/movies_app/views.py
+++ movies/movies_app/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 #Importing the application.
 from movies_app.models import Movie
 # Create your views here.
-#Creating a function.
+#Creating a function to handle requests.
 def movie_list(request):
