e160951a233755d8336a29740a724ed3336f67fb
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-25 14:27:03 -0400

    Created another charfield and assigned it to the variable description.

--- movies/movies_app/models.py
+++ movies/movies_app/models.py
@@ -7,3 +7,4 @@ class Movie(models.Model):
     # Creating a charfield and assigned it to the variable name.
     name = models.CharField(max_length=50)
     #Creating another charfield and assigned it to the variable description.
+    description = models.CharField(max_length=200)
