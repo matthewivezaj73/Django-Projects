27cbe03d21306b0b140256c16af070fa51462fc5
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-25 14:24:52 -0400

    added a comment.

--- movies/movies_app/models.py
+++ movies/movies_app/models.py
@@ -4,4 +4,5 @@ from django.db import models
 # Create your models here.
 #Creating a class.
 class Movie(models.Model):
+    # Creating a charfield and assigned it to the variable name.
     name = models.CharField(max_length=50)
