99bd30dcb9a9519ea5de671f89fa3eda8e43afd5
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-25 19:23:38 -0400

    Commit

--- movies/movies_app/urls.py
+++ movies/movies_app/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path, include
 from movies_app.views import movie_list
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('movie/', include('movies_app.urls')),
+    path('list/', movie_list, name='movie-list'),
 ]
